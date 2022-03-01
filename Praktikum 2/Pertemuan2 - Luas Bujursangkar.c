7dfb271f0030b60a357407dabe9ef0b4f590dffb
SHieon <89286782+phantomeon@users.noreply.github.com>
2022-03-01 11:23:11 +0700

    Add files via upload

---
+++ Praktikum 2/Pertemuan2 - Luas Bujursangkar.c
@@ -0,0 +1,22 @@
+/* Nama File    : LuasBujursangkar.c */
+/* Deskripsi    : Menghitung luas lingkaran */
+/* Pembuat      : Faris Naufal Izzatur Rahman - 24060121130055 */
+/* Tgl Pembuatan: Selasa, 01-03-2022 11:04 WIB */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/* Kamus */
+    int s;
+    int l;
+
+/* Algoritma */
+    s = 5;
+    l = s*s;
+
+    printf("Panjang sisi bujursangkar = %d\n", s);
+    printf("Luas Bujursangkar = %d", l);
+    return 0;
+}
